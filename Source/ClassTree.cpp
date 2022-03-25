2b920606f9ef6d4babbc995815defac61e0993ee
MrIllas <44169046+MrIllas@users.noreply.github.com>
2022-03-25 17:52:03 +0100

    Skill Tree Class base functions

--- Source/ClassTree.cpp
+++ Source/ClassTree.cpp
@@ -1,1 +1,60 @@
 #include "ClassTree.h"
+
+ClassTree::ClassTree()
+{
+	LoadTree();
+}
+
+ClassTree::~ClassTree()
+{
+
+}
+
+SkillTreeElement* ClassTree::getSkillTree(int value)
+{
+	return skillTree[value];
+}
+
+bool ClassTree::unlockSkill(int* classPoints, int skillId)
+{
+	SkillTreeElement* theSkill = getSkillTree(skillId);
+	if (theSkill == nullptr) return false;
+
+	SkillTreeElement* theRequiredSkill = getSkillTree(theSkill->requiresID);
+	if (theRequiredSkill == nullptr) return false;
+	
+	//Checks if the skill with the given id is already maxed(level)
+	if (theSkill->currentLevel < theSkill->maxLevel)
+	{
+		//Checks if the required skill is already unlocked
+		if (theRequiredSkill->currentLevel >= SkillLevel::BLUE)
+		{
+			//Checks if there are enough points to unlock
+			if (*classPoints <= theSkill->cost)
+			{   //Unlocks
+				theSkill->currentLevel = (SkillLevel) ((int)theSkill->currentLevel + 1);
+				classPoints -= theSkill->cost;
+				
+				return true;
+			}
+		}
+	}
+
+	//Return false means that there is nothing to unlock.
+	return false;
+}
+
+//bool ClassTree::levelUpSkill()
+//{
+//
+//}
+
+bool ClassTree::LoadTree()
+{
+	LoadBaseTree();
+}
+
+bool ClassTree::LoadBaseTree()
+{
+	skillTree[0] = new SkillTreeElement(0, "Cover", "Takes cover", 100, 0);
+}
--- Source/ClassTree.h
+++ Source/ClassTree.h
@@ -1,9 +1,60 @@
-#pragma once
+#ifndef __CLASS_TREE_H__
+#define __CLASS_TREE_H__
+#include <string>
+#define TREE_SIZE 10
 
+enum class SkillLevel {
+	LOCKED = 0,
+	GREEN,
+	BLUE,
+	PURPLE,
+	YELLOW
+};
+
+struct SkillTreeElement
+{
+	int id = 0;
+	std::string name = "";
+	std::string description = "";
+	int cost = 100;
+
+	int requiresID = 0;
+
+	SkillLevel currentLevel = SkillLevel::LOCKED;
+	SkillLevel maxLevel = SkillLevel::PURPLE;
+
+	SkillTreeElement(int id, std::string name, std::string description, int cost, int requiresID, SkillLevel maxLevel = SkillLevel::PURPLE)
+	{
+		this->id = id;
+		this->name = name;
+		this->description = description;
+		this->cost = cost;
+		this->requiresID = requiresID;
+
+		this->maxLevel = maxLevel;
+	};
+};
 
 
 class ClassTree
 {
+public:
 
+
+protected:
+	SkillTreeElement* skillTree[TREE_SIZE];
+
+public:
+	ClassTree();
+	~ClassTree();
+
+
+	SkillTreeElement* getSkillTree(int value);
+	bool unlockSkill(int* classPoints, int skillId);
+	bool levelUpCheck(SkillTreeElement* theSkill);
+	
+	bool LoadTree();
+	bool LoadBaseTree();
 };
 
+#endif
