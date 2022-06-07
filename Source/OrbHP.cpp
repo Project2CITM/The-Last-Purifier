fb6689be1025e40a8509cab84752a7617640f278
Roger <79161186+Draquian@users.noreply.github.com>
2022-06-07 19:33:38 +0200

    bug fixed

--- Source/OrbHP.cpp
+++ Source/OrbHP.cpp
@@ -79,14 +79,14 @@ void OrbHP::OnCollisionEnter(PhysBody* col)
 	}
 }
 
-void OrbHP::OnTriggerEnter(std::string trigger, PhysBody* col)
+/*void OrbHP::OnTriggerEnter(std::string trigger, PhysBody* col)
 {
 	if (trigger == "OrbHPTrigger" && col->gameObject->name == "Player")
 	{
 		PlusHP();
 		pendingToDelete = true;
 	}
-}
+}*/
 
 void OrbHP::GameEventTriggered(GameEvent id)
 {
