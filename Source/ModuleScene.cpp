e677943baa5e7120dcb35352ba47ab7ba90f039e
AdriaSeSa <76749150+AdriaSeSa@users.noreply.github.com>
2022-06-14 20:43:21 +0200

    Add player hit SFX

--- Source/ModuleScene.cpp
+++ Source/ModuleScene.cpp
@@ -21,7 +21,7 @@ ModuleScene::ModuleScene(bool start_enabled) : Module(start_enabled)
 	scenes[GAME_OVER] = new GameOverScene();
 	scenes[WIN] = new WinScene();
 
-	currentScene = WIN;
+	currentScene = LEVEL_1;
 }
 
 ModuleScene::~ModuleScene()
--- Source/PlayerController.h
+++ Source/PlayerController.h
@@ -137,7 +137,7 @@ private:
 
 	uint playerdodgeFX = NULL;
 
-	uint playerhitFX = NULL;
+	uint playerhitFX[3];
 
 	Timer playerTimer;
 
